8aae8bd1230341e30b40424d8d46f34cc2b5707a
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-10-09 12:01:33 -0300

    branch dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import Employee from "./components/employee/employee";
-import employee from "./assets/employee.svg";
-
 function App() {
-  const [showModal, setShowModal] = useState(true);
   return (
     <div className="w-screen h-screen bg-white flex items-center justify-center">
       <div className="relative min-w-[20.625rem] min-h-[43.625rem] bg-purple rounded-3xl flex items-center justify-center"></div>
