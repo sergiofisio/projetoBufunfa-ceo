fcee7ee970c296bc2cbf8fc78c4fee3b4112f1dc
Raul Barbosa <barbosamartins94@gmail.com>
2023-10-10 15:09:38 -0300

    feat: styled guide created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className="w-screen h-screen bg-white flex items-center justify-center">
-      <div className="relative min-w-[20.625rem] min-h-[43.625rem] bg-purple rounded-3xl flex items-center justify-center"></div>
+      <div className="relative min-w-[20.625rem] min-h-[43.625rem] bg-purpleDark rounded-3xl flex items-center justify-center gap-3">
+      </div>
     </div>
   );
 }
--- src/components/employee/employee.tsx
+++ src/components/employee/employee.tsx
@@ -12,12 +12,12 @@ export default function Employee({
       className={`flex items-center justify-between w-full p-4 ${classname}`}
     >
       <img
-        className="h-24 w-24 rounded-[100%] border-purple border-2"
+        className="h-24 w-24 rounded-[100%] border-purpleDark border-2"
         src={img}
         alt={`img ${name}`}
       />
       <h1 className="text-lg">{name}</h1>
-      <h2 className="w-8 h-8 text-white bg-purple flex items-center justify-center rounded-[100%] text-xl">
+      <h2 className="w-8 h-8 text-white bg-purpleDark flex items-center justify-center rounded-[100%] text-xl">
         X
       </h2>
     </div>
