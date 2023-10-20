58936089ef01adb11b67c22300819a4e55a9d2b2
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-10-20 09:20:01 -0300

    teste localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./routes.tsx";
 import { ToastContainer } from "react-toastify";
+// import { InfinitySpin } from "react-loader-spinner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <BrowserRouter>
+      {/* <InfinitySpin width="300" color="#00BFFF" /> */}
       <MainRoutes />
     </BrowserRouter>
     <ToastContainer />
